594d17c3662a405e2eaa697c685ef647ec7477a1
Thomas Hermann <novascreen@gmail.com>
2016-04-11 21:30:08 -0400

    refactored and improved demo

--- demo/src/index.js
+++ demo/src/index.js
@@ -6,6 +6,19 @@ import Column from '../../src'
 import LoremImage from '../../src'
 import styles from './styles.css'
 
+const dimensions = [
+  { width: 800, height: 1200 },
+  { width: 800, height: 600 },
+  { width: 800, height: 800 },
+  { width: 800, height: 600 },
+  { width: 800, height: 1200 },
+  { width: 800, height: 600 },
+  { width: 800, height: 400 },
+  { width: 800, height: 400 },
+  { width: 800, height: 400 },
+  { width: 800, height: 800 },
+]
+
 class Demo extends Component {
   constructor(props) {
     super(props)
@@ -19,20 +32,18 @@ class Demo extends Component {
     this.setState({ columns: random(1, 5) })
   }
 
+  renderItems() {
+    return dimensions.map(({ width, height }, i) => (
+      <div key={i} className={styles.demoItem} style={{
+        paddingTop: (height/width*100) + '%'
+      }}>
+        <img src={`http://placehold.it/${width}x${height}?text=${i+1}`} alt="" />
+      </div>
+    ))
+  }
+
   render() {
     const { columns } = this.state
-    const dimensions = [
-      { width: 800, height: 1200 },
-      { width: 800, height: 600 },
-      { width: 800, height: 800 },
-      { width: 800, height: 600 },
-      { width: 800, height: 1200 },
-      { width: 800, height: 600 },
-      { width: 800, height: 400 },
-      { width: 800, height: 400 },
-      { width: 800, height: 400 },
-      { width: 800, height: 800 },
-    ]
     const queries = [{
       columns: 2,
       query: 'min-width: 500px'
@@ -45,27 +56,21 @@ class Demo extends Component {
         <h1>react-columns Demo</h1>
         <p>By default react-columns iterates over the columns and spreads the children equally among them </p>
         <Column gap="20px" queries={queries}>
-          {dimensions.map(({ width, height }, i) => (
-            <img key={i} className={styles.demoItem} src={`http://placehold.it/${width}x${height}?text=${i+1}`} alt="" />
-          ))}
+          {this.renderItems()}
         </Column>
         <hr/>
         <p>If you pass the dimensions of every item react-columns iterates over the children and adds them to the shortest column. It uses the aspect ratio to calculate a comparable height.</p>
         <p>This also demonstrates a different value for the `gap` property. The vertical spacing is defined by the items' own styles.</p>
         <Column gap="10px" queries={queries} dimensions={dimensions}>
-          {dimensions.map(({ width, height }, i) => (
-            <img key={i} className={styles.demoItem} src={`http://placehold.it/${width}x${height}?text=${i+1}`} alt="" />
-          ))}
+          {this.renderItems()}
         </Column>
         <hr/>
         <p>You can also simply set the columns directly without any automatic media query handling</p>
         <p>
           <button type="button" onClick={this.onColumnsButtonClick}>Randomize columns</button>
         </p>
-        <Column gap="20px" columns={columns}>
-          {dimensions.map(({ width, height }, i) => (
-            <img key={i} className={styles.demoItem} src={`http://placehold.it/${width}x${height}?text=${i+1}`} alt="" />
-          ))}
+        <Column gap="20px" columns={columns} dimensions={dimensions}>
+          {this.renderItems()}
         </Column>
       </div>
     )
