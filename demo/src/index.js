dab66193a405838f158250c28906207d27d32b09
Thomas Hermann <novascreen@gmail.com>
2017-01-07 14:13:30 -0500

    Rename Column to Columns import in demo
    It should reflect the module name.

--- demo/src/index.js
+++ demo/src/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import {render} from 'react-dom'
 import random from 'lodash.random'
 
-import Column from '../../src'
+import Columns from '../../src'
 import LoremImage from '../../src'
 import styles from './styles.css'
 
@@ -55,23 +55,23 @@ class Demo extends Component {
       <div>
         <h1>react-columns Demo</h1>
         <p>By default react-columns iterates over the columns and spreads the children equally among them </p>
-        <Column gap="20px" queries={queries}>
+        <Columns gap="20px" queries={queries}>
           {this.renderItems()}
-        </Column>
+        </Columns>
         <hr/>
         <p>If you pass the dimensions of every item react-columns iterates over the children and adds them to the shortest column. It uses the aspect ratio to calculate a comparable height.</p>
         <p>This also demonstrates a different value for the `gap` property. The vertical spacing is defined by the items' own styles.</p>
-        <Column gap="10px" queries={queries} dimensions={dimensions}>
+        <Columns gap="10px" queries={queries} dimensions={dimensions}>
           {this.renderItems()}
-        </Column>
+        </Columns>
         <hr/>
         <p>You can also simply set the columns directly without any automatic media query handling</p>
         <p>
           <button type="button" onClick={this.onColumnsButtonClick}>Randomize columns</button>
         </p>
-        <Column gap="20px" columns={columns} dimensions={dimensions}>
+        <Columns gap="20px" columns={columns} dimensions={dimensions}>
           {this.renderItems()}
-        </Column>
+        </Columns>
       </div>
     )
   }
